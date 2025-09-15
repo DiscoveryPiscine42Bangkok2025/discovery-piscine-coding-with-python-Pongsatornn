bac4f958130338e7ef2924f0b31368054c78b700
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-15 23:13:01 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,14 @@
+import sys
+import re
+params = sys.argv[1:]
+if len(params) != 2:
+    print("none")
+else:
+    x = params[0]
+    text = params[1]
+    ans = re.findall(x, text)
 
+    if len(ans) == 0:
+        print("none")
+    else:
+        print(len(ans))
