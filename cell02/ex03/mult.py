06b82cd22a720dafedd06ee605050d01fbacd8ea
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-15 18:12:43 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,10 @@
-
+first_number = int(input("Enter the first number : "))
+second_number = int(input("Enter the first number : "))
+mult  = first_number*second_number
+print(f"{first_number} x {second_number} = {mult}")
+if mult < 0 :
+    print("This number is negative.")
+elif mult > 0:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
