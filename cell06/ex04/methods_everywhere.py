06335e0f991c3972554dd4f4b81a5a92c967c3e0
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-16 00:38:17 +0700

    Update methods_everywhere.py

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,1 +1,20 @@
 
+import sys
+def shrink(i):
+    return i[:8]
+
+def enlarge(j):
+    return j + "Z" * (8 - len(j))
+
+x = sys.argv[1:]
+
+if len(x) < 1:
+    print("none")
+else:
+    for p in x:
+        if len(p) > 8:
+            print(shrink(p))
+        elif len(p) < 8:
+            print(enlarge(p))
+        else: 
+            print(p)
