b5ff66da9baf1edc44c05144f3c0381dc34aa601
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-15 23:23:43 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,10 @@
-
+import sys
+if len(sys.argv) != 2:
+    print("none")
+else:
+    param = sys.argv[1]
+    uinput = input("What was the parameter? ")
+    if uinput == param:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
