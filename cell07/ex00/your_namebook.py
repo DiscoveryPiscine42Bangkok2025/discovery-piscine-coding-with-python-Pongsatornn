ad3ae93b52ae91e6d0dcb85c40cdc718b5a0f0d0
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-16 00:59:27 +0700

    Update your_namebook.py

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,1 +1,15 @@
+def array_of_names(p):
+    name = []
+    for first,last in p.items():
+        full_name = first.capitalize() + " " + last.capitalize()
+        name.append(full_name)
+    return name
 
+pdas = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "miel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(pdas))
