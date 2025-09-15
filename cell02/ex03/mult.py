63c22bfe84a66be8325da61b422dcd8c839ab369
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-15 18:12:57 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,5 +1,5 @@
 first_number = int(input("Enter the first number : "))
-second_number = int(input("Enter the first number : "))
+second_number = int(input("Enter the second number : "))
 mult  = first_number*second_number
 print(f"{first_number} x {second_number} = {mult}")
 if mult < 0 :
