ebe876ecf82572ec32733e888f994e8630917025
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-15 21:29:41 +0700

    Update age.py

--- cell04/ex1/age.py
+++ cell04/ex1/age.py
@@ -1,1 +1,8 @@
+age = int(input("Please tell me your age:"))
+print(f"You are currently {age} years old.")
+i = 1  
+while i <= 3:
+    print(f"in {i * 10} years, you'll be {(i * 10 )+ age} years old.")
+    i+=1
+
 
