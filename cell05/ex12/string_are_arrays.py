11c6a6416babb7f09d02fbfd12a3ae62890c5269
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-15 23:41:43 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,13 @@
-
+import sys
+if len(sys.argv) != 2:
+    print("none")
+else:
+    text = sys.argv[1]
+    result = ""
+    for ch in text:
+        if ch == "z":   
+            result += "z"
+    if result == "":
+        print("none")
+    else:
+        print(result)
