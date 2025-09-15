2690580bd4b134e6cce0f152088c89789f6e8b4c
Pongsatorn._. <pongsatornazsx1234@gmail.com>
2025-09-16 01:19:45 +0700

    Update family_affairs.py

--- cell07/ex01/family_affairs.py
+++ cell07/ex01/family_affairs.py
@@ -1,1 +1,20 @@
+def is_redhead(name):
+    return family[name] == "red"
+
+def find_the_redheads(x):
+    global family
+    family = x 
+    red = filter(is_redhead, family.keys())
+    return list(red)
+
+
+dupont_family = {
+    "florian": "red",
+    "marie": "blond",
+    "virginie": "brunette",
+    "david": "red",
+    "franck": "red"
+}
+
+print(find_the_redheads(dupont_family))
 
